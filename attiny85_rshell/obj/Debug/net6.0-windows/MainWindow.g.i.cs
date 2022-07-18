9b70a72a7955181134db73c4b4a7fca1254e01d6
logan <loganmitchell2011@gmail.com>
2022-07-17 23:34:11 -0400

    Delete everything in obj.

--- attiny85_rshell/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ attiny85_rshell/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -309,7 +309,7 @@ namespace attiny85_rshell {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/attiny85_rshell;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/attiny85_rshell;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
