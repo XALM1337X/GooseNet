86273ae1ceaaa1c2c0614cba6dd6853175c2354e
logan <loganmitchell2011@gmail.com>
2022-07-26 00:54:05 -0400

    lots of front end and backend changes.

--- attiny85_rshell/MainWindow.xaml.cs
+++ attiny85_rshell/MainWindow.xaml.cs
@@ -16,16 +16,19 @@ using System.Text.Json;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace attiny85_rshell { 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        public int GlobalServerID { get; set; }
+
+
         public MainWindow() {
             InitializeComponent();
-            
-
         }
 
         private void MasterClientConfigureView(object sender, RoutedEventArgs e) {
@@ -65,13 +68,15 @@ namespace attiny85_rshell {
             Panel.SetZIndex(landing_page, 0);
             Panel.SetZIndex(server_configuration_canvas, 1);
         }
+
         private void ServerConfBackClick(object sender, RoutedEventArgs e) {
             Panel.SetZIndex(landing_page, 1);
             Panel.SetZIndex(server_configuration_canvas, 0);
         }
+
         private void ServerConfSubmit(object sender, RoutedEventArgs e) {
-            if (File.Exists("../../../data/rs_server.ps1")) {
-                string question = "../../../data/rs_server.ps1 already exists. Would you like to overwrite?";
+            if (File.Exists("../../../scripts/rs_server.ps1")) {
+                string question = "../../../scripts/rs_server.ps1 already exists. Would you like to overwrite?";
                 if (MessageBox.Show(question, "File Exists", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No) {
                     return;
                 }
@@ -108,14 +113,14 @@ namespace attiny85_rshell {
                     return;
                 } 
                 if (changes_made) {
-                    using (StreamWriter writetext = new StreamWriter("../../../data/rs_server.ps1")) {
+                    using (StreamWriter writetext = new StreamWriter("../../../scripts/rs_server.ps1")) {
                         foreach (string line in lines) {
                             writetext.WriteLine(line);
                         }
                         writetext.Close();
                     }
                     FlowDocument myFlowDoc = new FlowDocument();
-                    myFlowDoc.Blocks.Add(new Paragraph(new Run("Succesfully wrote: ../../../data/rs_server.ps1")));
+                    myFlowDoc.Blocks.Add(new Paragraph(new Run("Succesfully wrote: ../../../scripts/rs_server.ps1")));
                     landing_page_log.Document.Blocks.Clear();
                     landing_page_log.Document = myFlowDoc;
                 }            
@@ -131,27 +136,66 @@ namespace attiny85_rshell {
             
         }
 
-        private void PayloadConfigureButtonClick(object sender, RoutedEventArgs e)
-        {
+        private void PayloadConfigureButtonClick(object sender, RoutedEventArgs e) {
             Panel.SetZIndex(landing_page, 0);
             Panel.SetZIndex(payload_conf_canvas, 1);
         }
 
+        private void PayloadConfigSubmit(object sender, RoutedEventArgs e) {
+            //FolderBrowserDialog folderBrowserDialog1;
+            //folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            //folderBrowserDialog1.Description ="Select the directory that you want to use as the default.";
+            //folderBrowserDialog1.ShowNewFolderButton = false;
+            //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;
+
+            //Set Hosting domains for task and slave
+            //Set Slave Target server and port
+        }
+
         private void RunServerClick(object sender, RoutedEventArgs e) {
-            if (File.Exists("..\\..\\..\\data\\rs_server.ps1")) {
+            if (File.Exists("..\\..\\..\\scripts\\rs_server.ps1")) {
                 using (Process myProcess = new Process()) {
+
+                    myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
                     myProcess.StartInfo.UseShellExecute = true;
                     // You can start any process, HelloWorld is a do-nothing example.
                     myProcess.StartInfo.FileName = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
-                    myProcess.StartInfo.CreateNoWindow = false;
-                    myProcess.StartInfo.Arguments = "..\\..\\..\\data\\rs_server.ps1";
+                    myProcess.StartInfo.CreateNoWindow = true;
+                    myProcess.StartInfo.Arguments = "..\\..\\..\\scripts\\rs_server.ps1";
                     myProcess.Start();
+                    GlobalServerID = myProcess.Id;
+
+                    FlowDocument myFlowDoc = new FlowDocument();
+                    myFlowDoc.Blocks.Add(new Paragraph(new Run("Server Started Succesfully.")));
+                    myFlowDoc.Blocks.Add(new Paragraph(new Run("Server ID: "+ GlobalServerID.ToString())));
+                    landing_page_log.Document.Blocks.Clear();
+                    landing_page_log.Document = myFlowDoc;
+
+
                 }
             } else {
-                MessageBox.Show("..\\..\\..\\data\\rs_server.ps1 does not exist. Use server configuration button.","Configured server not found", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("..\\..\\..\\scripts\\rs_server.ps1 does not exist. Use server configuration button.","Configured server not found", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
         }
+
+        private void KillServerClick(object sender, RoutedEventArgs e) {
+
+            Process myProcess = Process.GetProcessById(GlobalServerID);
+            if (myProcess != null) {
+                myProcess.CloseMainWindow();
+                myProcess.Close();
+                FlowDocument myFlowDoc = new FlowDocument();
+                myFlowDoc.Blocks.Add(new Paragraph(new Run("Server shutdown succesfully.")));
+                landing_page_log.Document.Blocks.Clear();
+                landing_page_log.Document = myFlowDoc;
+            } else {
+                MessageBox.Show("Failed to shutdown server process.", "Fail", MessageBoxButton.OK, MessageBoxImage.Error);
+                return ;
+            }
+        }
+
+
     }
 }
